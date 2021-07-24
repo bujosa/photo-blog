e479f072d717465360f14fdeb1f0f0ac68564154
bujosa <davidbujosa@gmail.com>
2021-07-24 05:10:59 -0400

    Added git ignore

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"crypto/sha1"
 	"fmt"
-	"github.com/satori/go.uuid"
 	"html/template"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 var tpl *template.Template
@@ -64,7 +65,7 @@ func index(w http.ResponseWriter, req *http.Request) {
 func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
 	c, err := req.Cookie("session")
 	if err != nil {
-		sID, _ := uuid.NewV4()
+		sID := uuid.NewV4()
 		c = &http.Cookie{
 			Name:  "session",
 			Value: sID.String(),
